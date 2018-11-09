bb414d4bf5c5872deca73f6e9976fd2973560289
psvuyyuri <44709471+psvuyyuri@users.noreply.github.com>
2018-11-09 08:49:15 -0500

    TestCode

---
+++ Code/Sample.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  9 08:47:14 2018
+
+@author: vuyyuri
+"""
+
+print("Sample")
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # IU-Python-G19
 IU-Python-G19 Final Project
+Created By Satish Vuyyuri on 11/09/18
